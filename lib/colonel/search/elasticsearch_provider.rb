f97d703d0caf07596401c258508851e264be3c45
Alex Young <Alexander.Young@sky.uk>
2015-12-01 15:26:37 +0000

    Match type definitions between mappings

--- lib/colonel/search/elasticsearch_provider.rb
+++ lib/colonel/search/elasticsearch_provider.rb
@@ -230,10 +230,12 @@ module Colonel
             # _id is "{id}-{state}"
             id: {
               type: 'string',
+              store: 'yes',
               index: 'not_analyzed'
             },
             state: {
               type: 'string',
+              store: 'yes',
               index: 'not_analyzed'
             },
             updated_at: {
